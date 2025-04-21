27d2909fd01810008485ccaf018534b56cde40c2
cercil0605 <kuamaichi2223@gmail.com>
2025-04-21 17:52:25 +0900

    add: contact button

--- src/app/page.js
+++ src/app/page.js
@@ -121,9 +121,21 @@ export default async function Home() {
           </h3>
           <TimelineSection experience={experience} />
         </section>
+        {/* contact */}
+        <section id="contact" className="text-center pt-12 text-[#555] space-y-2">
+          <h3 className="text-3xl font-extrabold mb-6 border-black pb-2">Interested in working together?</h3>
+          <a
+  href="mailto:support@cercil.net"
+  className="inline-block bg-black text-white px-6 py-2 rounded-md font-semibold hover:bg-gray-800 transition"
+>
+  support@cercil.net
+</a>
 
+
+
+        </section>
         {/* Footer */}
-        <section id="contact" className="text-center pt-12 text-[#555]">
+        <section id="footer" className="text-center pt-12 text-[#555]">
           <p>© 2025 Keisuke Shibuya</p>
         </section>
       </main>
